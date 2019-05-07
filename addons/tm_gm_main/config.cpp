70e48b6c68dec6d56996175774ff14b8ec9c16c9
Bear <nipronz@gmail.com>
2019-05-07 10:35:04 +0200

    main

---
+++ addons/tm_gm_main/config.cpp
@@ -0,0 +1,15 @@
+#include "script_component.hpp"
+
+class cfgPatches {
+	class ADDON {
+        name = "1Tac Global Mobilization: Main";
+		author = "1Tac";
+		url = "http://www.teamonetactical.com";
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = REQUIRED_VERSION;
+		/* Require CBA and all components below */
+		requiredAddons[] = {"tac1_tm_misc"};
+		VERSION_CONFIG;
+	};
+};
---
+++ addons/tm_gm_main/script_component.hpp
@@ -0,0 +1,4 @@
+#define COMPONENT tm_gm_main
+
+#include "\x\tac1\addons\tm_gm_main\script_mod.hpp"
+#include "\x\tac1\addons\tm_gm_main\script_macros.hpp"
